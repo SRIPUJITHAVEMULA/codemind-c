7bc64d1cfc9d73a086edd06d041014e2aa875828
SRIPUJITHAVEMULA <119916263+SRIPUJITHAVEMULA@users.noreply.github.com>
2022-12-07 10:07:17 +0530

    #include<stdio.h> int main() {     int n,r=0,re,o;     scanf("%d",&n);     o=n;     while(n!=0)     {         re=n%10;         r=r*10+re;         n/=10;     }     if(o==r)     {         printf("Palindrome");     }     else     {         printf("Not Palindrome");     }
    }

---
+++ Palindrome.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,r=0,re,o;
+    scanf("%d",&n);
+    o=n;
+    while(n!=0)
+    {
+        re=n%10;
+        r=r*10+re;
+        n/=10;
+    }
+    if(o==r)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
+
+
+
+    
+}
