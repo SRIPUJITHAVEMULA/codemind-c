8e988464529c0219c53414727a1b3804be8491d0
SRIPUJITHAVEMULA <119916263+SRIPUJITHAVEMULA@users.noreply.github.com>
2023-01-11 21:42:26 +0530

    #include<stdio.h> int prime(int n); int main() {     int r,i;     scanf("%d",&r);     for(i=1;i<=r;i++)     {         int n,d1,d2,ap,bp,i;         scanf("%d",&n);         for(i=n;;i++)         {             if(prime(i))             {                 ap=i;                 d1=i-n;                 break;             }         }         for(i=n;;i--)         {             if(prime(i))             {                 bp=i;                 d2=n-i;                 break;             }         }         if(d1<d2)         {             printf("%d ",ap);         }         else if(d1>d2)         {             printf("%d ",bp);         }         else         {             printf("%d ",bp);         }     } }     int prime(int n)     {         int i,c=0;         for(i=1;i<=n;i++)         {             if(n%i==0)             {                 c++;             }         }         if(c==2)         {             return 1;         }         else         {             return 0;         }     }

---
+++ Nearest_Prime.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int prime(int n);
+int main()
+{
+    int r,i;
+    scanf("%d",&r);
+    for(i=1;i<=r;i++)
+    {
+        int n,d1,d2,ap,bp,i;
+        scanf("%d",&n);
+        for(i=n;;i++)
+        {
+            if(prime(i))
+            {
+                ap=i;
+                d1=i-n;
+                break;
+            }
+        }
+        for(i=n;;i--)
+        {
+            if(prime(i))
+            {
+                bp=i;
+                d2=n-i;
+                break;
+            }
+        }
+        if(d1<d2)
+        {
+            printf("%d
+",ap);
+        }
+        else if(d1>d2)
+        {
+            printf("%d
+",bp);
+        }
+        else
+        {
+            printf("%d
+",bp);
+        }
+    }
+}
+    int prime(int n)
+    {
+        int i,c=0;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
